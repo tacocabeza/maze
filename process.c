297dde89dd4a3c8e962f8da608570b2c20bafbc4
j <jgarcia6102@gmail.com>
2024-12-20 23:22:12 -0700

    read file

--- process.c
+++ process.c
@@ -1,4 +1,15 @@
 #include "process.h"
 #include "stdio.h"
 
-void read_png(char *filename) { printf("reading file: %s\n", filename); }
+void read_png(char *filename) {
+  printf("reading file: %s\n", filename);
+
+  FILE *file;
+
+  if ((file = fopen(filename, "r"))) {
+    fclose(file);
+    printf("Read File %s\n", filename);
+  } else {
+    printf("File %s does not exist!\n", filename);
+  }
+}
